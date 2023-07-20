08a6adf0f14c2c39c73e84776820768adc9739df
Adrian Wróblewski <xwrobel02@gmail.com>
2023-07-20 15:21:11 +0200

    Update pusherClient.ts

--- frontend/src/app/pusherClient.ts
+++ frontend/src/app/pusherClient.ts
@@ -4,6 +4,6 @@ export const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
     cluster: import.meta.env.VITE_PUSHER_CLUSTER,
     userAuthentication: {
         transport: "jsonp",
-        endpoint: import.meta.env.VITE_API_URL,
+        endpoint: `${import.meta.env.VITE_API_URL}/auth/pusher`,
     },
 })
